2f6fa6626d045999dfe0bcd8d52332699a7c7b5d
taskinerhan <erhantaskin184@gmail.com>
2025-04-15 00:31:54 +0300

    Inıtial commit

---
+++ src/main/java/com/example/git_deneme/GitDenemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_deneme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDenemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDenemeApplication.class, args);
+	}
+
+}
